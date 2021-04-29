f572e004b938a37ff6bed8f79e87648acd18d5c5
Frédéric Barthelet <fredericb@theodo.fr>
2021-04-29 09:49:57 +0200

    Add logicalId resolver for test suite

--- test/unit/storage.test.ts
+++ test/unit/storage.test.ts
@@ -2,12 +2,12 @@ import { pluginConfigExt, runServerless } from "../utils/runServerless";
 
 describe("storage", () => {
     it("should create an S3 bucket", async () => {
-        const { cfTemplate } = await runServerless({
+        const { cfTemplate, computeLogicalId } = await runServerless({
             fixture: "storage",
             configExt: pluginConfigExt,
             cliArgs: ["package"],
         });
-        expect(cfTemplate.Resources.storageavatarsBucketEA65C381).toMatchObject({
+        expect(cfTemplate.Resources[computeLogicalId("storage", "avatars", "Bucket")]).toMatchObject({
             Type: "AWS::S3::Bucket",
         });
     });
--- test/utils/runServerless.ts
+++ test/utils/runServerless.ts
@@ -1,10 +1,42 @@
 import path from "path";
+import { ConstructNode, Names } from "@aws-cdk/core";
+import type originalRunServerless from "@serverless/test/run-serverless";
 import setupRunServerlessFixturesEngine from "@serverless/test/setup-run-serverless-fixtures-engine";
 
-export const runServerless = setupRunServerlessFixturesEngine({
-    fixturesDir: path.resolve(__dirname, "../fixtures"),
-    serverlessDir: path.resolve(__dirname, "../../node_modules/serverless"),
-});
+type ComputeLogicalId = (...address: string[]) => string;
+
+type RunServerlessPromiseReturn = ReturnType<typeof originalRunServerless>;
+type ThenArg<T> = T extends PromiseLike<infer U> ? U : T;
+type RunServerlessReturn = ThenArg<RunServerlessPromiseReturn>;
+
+const computeLogicalId = (serverless: Record<string, unknown>, ...address: string[]): string => {
+    // @ts-expect-error Comes from the stack being declared by plugin.ts. Serverless context object definition shall be improved.
+    const initialNode = serverless.stack.node as ConstructNode;
+    const foundNode = [...address, "Resource"].reduce((currentNode, nextNodeId) => {
+        const nextNode = currentNode.tryFindChild(nextNodeId);
+        if (!nextNode) {
+            throw new Error(`No node named ${nextNodeId} found in ${address.join(".")} address.`);
+        }
+
+        return nextNode.node;
+    }, initialNode);
+
+    return Names.nodeUniqueId(foundNode);
+};
+
+export const runServerless = async (
+    options: Parameters<typeof originalRunServerless>[0]
+): Promise<RunServerlessReturn & { computeLogicalId: ComputeLogicalId }> => {
+    const runServerlessReturnValues = await setupRunServerlessFixturesEngine({
+        fixturesDir: path.resolve(__dirname, "../fixtures"),
+        serverlessDir: path.resolve(__dirname, "../../node_modules/serverless"),
+    })(options);
+
+    return {
+        ...runServerlessReturnValues,
+        computeLogicalId: (...address: string[]) => computeLogicalId(runServerlessReturnValues.serverless, ...address),
+    };
+};
 
 export const pluginConfigExt = {
     plugins: [path.join(process.cwd(), "src/plugin.ts")],
