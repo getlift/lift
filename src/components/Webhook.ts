304cf89d368081e08ca9b320d014f865eba7a967
Frédéric Barthelet <fredericb@theodo.fr>
2021-05-06 12:41:38 +0200

    Add CDK event bridge

--- src/components/Webhook.ts
+++ src/components/Webhook.ts
@@ -1,6 +1,7 @@
 import { Construct, Fn } from "@aws-cdk/core";
 import { CfnIntegration, CfnRoute, HttpApi, HttpMethod } from "@aws-cdk/aws-apigatewayv2";
 import { HttpProxyIntegration } from "@aws-cdk/aws-apigatewayv2-integrations";
+import { EventBus } from "@aws-cdk/aws-events";
 import { FromSchema } from "json-schema-to-ts";
 import { PolicyDocument, PolicyStatement, Role, ServicePrincipal } from "@aws-cdk/aws-iam";
 import type { Serverless } from "../types/serverless";
@@ -41,8 +42,22 @@ export class Webhook extends Component<typeof WEBHOOK_COMPONENT, typeof WEBHOOK_
         const webhookConfigurations = Object.entries(this.getConfiguration());
         if (webhookConfigurations.length !== 0) {
             const api = new HttpApi(this, "HttpApi");
+            const bus = new EventBus(this, "Bus");
+            const apiGatewayRole = new Role(this, "ApiGatewayRole", {
+                assumedBy: new ServicePrincipal("apigateway.amazonaws.com"),
+                inlinePolicies: {
+                    EventBridge: new PolicyDocument({
+                        statements: [
+                            new PolicyStatement({
+                                actions: ["events:PutEvents"],
+                                resources: [bus.eventBusArn],
+                            }),
+                        ],
+                    }),
+                },
+            });
             webhookConfigurations.map(([webhookName, webhookConfiguration]) => {
-                new WebhookConstruct(this, webhookName, this.serverless, api, webhookConfiguration);
+                new WebhookConstruct(this, webhookName, this.serverless, api, bus, apiGatewayRole, webhookConfiguration);
             });
         }
     }
@@ -54,6 +69,8 @@ class WebhookConstruct extends ComponentConstruct {
         id: string,
         serverless: Serverless,
         api: HttpApi,
+        bus: EventBus,
+        apiGatewayRole: Role,
         webhookConfiguration: FromSchema<typeof WEBHOOK_DEFINITION>
     ) {
         super(scope, id, serverless);
@@ -68,19 +85,6 @@ class WebhookConstruct extends ComponentConstruct {
             integration,
         });
 
-        const apiGatewayRole = new Role(this, "ApiGatewayRole", {
-            assumedBy: new ServicePrincipal("apigateway.amazonaws.com"),
-            inlinePolicies: {
-                EventBridge: new PolicyDocument({
-                    statements: [
-                        new PolicyStatement({
-                            actions: ["events:PutEvents"],
-                            resources: ["*"],
-                        }),
-                    ],
-                }),
-            },
-        });
         const eventBridgeIntegration = new CfnIntegration(this, "Integration", {
             apiId: api.apiId,
             connectionType: "INTERNET",
@@ -92,6 +96,7 @@ class WebhookConstruct extends ComponentConstruct {
                 DetailType: "$request.body.payload",
                 Detail: "$request.body.type",
                 Source: "Webhook",
+                EventBusName: bus.eventBusName,
             },
         });
         new CfnRoute(this, "Route", {
