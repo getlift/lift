46d67cd662ac960bc75dc5de1368cfbec0fbf890
Matthieu Napoli <matthieu@mnapoli.fr>
2021-05-26 16:41:09 +0200

    Cleanup

--- src/CloudFormation.ts
+++ src/CloudFormation.ts
@@ -1,5 +1,4 @@
 import { DescribeStacksInput, DescribeStacksOutput } from "aws-sdk/clients/cloudformation";
-import { availabilityZones } from "./Zones";
 import { AwsProvider } from "./constructs/Provider";
 
 export async function getStackOutput(aws: AwsProvider, output: string): Promise<string | undefined> {
@@ -29,48 +28,4 @@ export async function getStackOutput(aws: AwsProvider, output: string): Promise<
     }
 
     return undefined;
-}
-
-export function cfRef(resource: string): { Ref: string } {
-    return { Ref: resource };
-}
-
-export function cfGetAtt(resource: string, attribute: string): Record<string, [string, string]> {
-    return {
-        "Fn::GetAtt": [resource, attribute],
-    };
-}
-
-export function cfJoin(glue: string, strings: Array<unknown>): Record<string, [string, Array<unknown>]> {
-    return {
-        "Fn::Join": [glue, strings],
-    };
-}
-
-export function cfSub(string: string): Record<string, string> {
-    return {
-        "Fn::Sub": string,
-    };
-}
-
-type CloudFormationTag = {
-    Key: string;
-    Value: string | Record<string, unknown>;
-};
-
-export function cfTag(key: string, value: string | Record<string, unknown>): CloudFormationTag {
-    return {
-        Key: key,
-        Value: value,
-    };
-}
-
-export function getAvailabilityZones(region: string): string[] {
-    const allZones = availabilityZones as Record<string, string[]>;
-
-    return (
-        allZones[region]
-            // Keep maximum 3 zones
-            .slice(0, 3)
-    );
 }
