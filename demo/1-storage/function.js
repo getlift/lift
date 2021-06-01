71631e4535e569d403fa39268fb74f7d25be84f1
Matthieu Napoli <matthieu@mnapoli.fr>
2021-06-01 10:53:16 +0200

    Improve demo app

--- demo/1-storage/function.js
+++ demo/1-storage/function.js
@@ -1,3 +1,20 @@
-exports.hello = async function(event, context) {
-    return process.env.BUCKET_NAME;
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+exports.handler = async function(event, context) {
+    const bucketname = process.env.BUCKET_NAME;
+    console.log(`Using bucket ${bucketname}`);
+
+    const keys = [];
+    const response = await s3.listObjectsV2({
+        Bucket: bucketname,
+    }).promise();
+    response.Contents.forEach(obj => keys.push(obj.Key));
+    console.log('Found keys:');
+    console.log(keys);
+
+    return {
+        bucket: bucketname,
+        keys: keys,
+    };
 }
