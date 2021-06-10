2c63302d7499ad2df3dd6fb0d90f48f782fd6afe
Frédéric Barthelet <fredericb@theodo.fr>
2021-06-10 22:41:05 +0200

    Add unit tests

--- test/unit/webhooks.test.ts
+++ test/unit/webhooks.test.ts
@@ -15,13 +15,44 @@ describe("webhooks", () => {
     });
     it("should allow insecure webhook", async () => {
         const { cfTemplate, computeLogicalId } = await runServerless({
-            fixture: "webhooksInsecure",
+            fixture: "webhooks",
             configExt: pluginConfigExt,
             cliArgs: ["package"],
         });
         expect(cfTemplate.Resources[computeLogicalId("github", "Route")]).toMatchObject({
             Properties: {
                 AuthorizationType: "NONE",
+            },
+        });
+    });
+
+    it("should generate only a single bus", async () => {
+        const { cfTemplate, computeLogicalId } = await runServerless({
+            fixture: "webhooks",
+            configExt: pluginConfigExt,
+            cliArgs: ["package"],
+        });
+        expect(cfTemplate.Resources[computeLogicalId("Bus")]).toMatchObject({
+            Properties: {
+                Name: "Bus",
+            },
+        });
+        expect(cfTemplate.Resources[computeLogicalId("github", "Integration")]).toMatchObject({
+            Properties: {
+                RequestParameters: {
+                    EventBusName: {
+                        Ref: computeLogicalId("Bus"),
+                    },
+                },
+            },
+        });
+        expect(cfTemplate.Resources[computeLogicalId("stripe", "Integration")]).toMatchObject({
+            Properties: {
+                RequestParameters: {
+                    EventBusName: {
+                        Ref: computeLogicalId("Bus"),
+                    },
+                },
             },
         });
     });
