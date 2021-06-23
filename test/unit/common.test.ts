b2cbd0cd9daab2761c8f0ebe5a47d7ff658d747a
Matthieu Napoli <matthieu@mnapoli.fr>
2021-06-23 16:14:28 +0200

    Improve tests

--- test/unit/common.test.ts
+++ test/unit/common.test.ts
@@ -1,5 +1,4 @@
 import { baseConfig, pluginConfigExt, runServerless } from "../utils/runServerless";
-import { runServerlessCli } from "../utils/runServerlessCli";
 
 describe("common", () => {
     it("should not override user defined resources in serverless.yml", async () => {
@@ -54,57 +53,5 @@ describe("common", () => {
                 }),
             })
         ).rejects.toThrow("Configuration error at 'constructs.avatars': unsupported value");
-    });
-
-    it("should resolve variables", async () => {
-        const { cfTemplate } = await runServerlessCli({
-            fixture: "variables",
-            command: "package",
-        });
-        // Resolves construct variables in `functions`
-        expect(cfTemplate.Resources.FooLambdaFunction).toMatchObject({
-            Properties: {
-                Environment: {
-                    Variables: {
-                        VAR1: {
-                            Ref: "barQueueB989EBF4",
-                        },
-                    },
-                },
-            },
-        });
-        // Resolves Framework variables in `constructs`
-        expect(cfTemplate.Resources.BarWorkerLambdaFunction).toMatchObject({
-            Properties: {
-                Environment: {
-                    Variables: {
-                        VAR1: "bar",
-                        CUSTOM_VAR1: "Custom variable 1",
-                        CUSTOM_VAR2: "Custom variable 2",
-                        CUSTOM_VAR3: "Custom variable 3",
-                        CUSTOM_VAR4: "Custom variable 4",
-                    },
-                },
-            },
-        });
-        expect(cfTemplate.Resources.appCDN7AD2C001).toMatchObject({
-            Properties: {
-                DistributionConfig: {
-                    Aliases: ["Custom variable 1"],
-                    ViewerCertificate: {
-                        AcmCertificateArn:
-                            "arn:aws:acm:us-east-1:123466615250:certificate/abcdef-b896-4725-96e3-6f143d06ac0b",
-                    },
-                },
-            },
-        });
-        // Resolves construct variables in `resources`
-        expect(cfTemplate.Resources.UserDefinedResource).toMatchObject({
-            Properties: {
-                BucketName: {
-                    Ref: "barQueueB989EBF4",
-                },
-            },
-        });
     });
 });
---
+++ test/unit/variables.test.ts
@@ -0,0 +1,53 @@
+import { runServerlessCli } from "../utils/runServerlessCli";
+
+describe("variables", () => {
+    it("should resolve construct variables", async () => {
+        const { cfTemplate } = await runServerlessCli({
+            fixture: "variables",
+            command: "package",
+        });
+        // Resolves construct variables in `functions`
+        expect(cfTemplate.Resources.FooLambdaFunction).toHaveProperty("Properties.Environment.Variables.VAR1", {
+            Ref: "barQueueB989EBF4",
+        });
+        // Resolves construct variables in `resources`
+        expect(cfTemplate.Resources.UserDefinedResource).toHaveProperty("Properties.BucketName", {
+            Ref: "barQueueB989EBF4",
+        });
+        // Resolves construct variables in `custom`
+        expect(cfTemplate.Resources.FooLambdaFunction).toHaveProperty("Properties.Environment.Variables.CUSTOM_VAR", {
+            Ref: "bucketBucketF19722A9",
+        });
+    });
+
+    it("should resolve variables in constructs", async () => {
+        const { cfTemplate } = await runServerlessCli({
+            fixture: "variables",
+            command: "package",
+        });
+        expect(cfTemplate.Resources.BarWorkerLambdaFunction).toHaveProperty("Properties.Environment.Variables", {
+            // Native serverless variable
+            VAR1: "bar",
+            // Custom variables defined by plugins (using a different API every time
+            CUSTOM_VAR1: "Custom variable 1",
+            CUSTOM_VAR2: "Custom variable 2",
+            CUSTOM_VAR3: "Custom variable 3",
+            CUSTOM_VAR4: "Custom variable 4",
+        });
+        // ${construct:bucket.bucketName} should have been resolved
+        expect(cfTemplate.Resources.barAlarmTopicSubscription56286022).toHaveProperty("Properties.Endpoint", {
+            Ref: "bucketBucketF19722A9",
+        });
+        expect(cfTemplate.Resources.appCDN7AD2C001).toMatchObject({
+            Properties: {
+                DistributionConfig: {
+                    Aliases: ["Custom variable 1"],
+                    ViewerCertificate: {
+                        AcmCertificateArn:
+                            "arn:aws:acm:us-east-1:123466615250:certificate/abcdef-b896-4725-96e3-6f143d06ac0b",
+                    },
+                },
+            },
+        });
+    });
+});
