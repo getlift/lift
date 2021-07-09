371affa18568b11ab4dd7605706439eb97684522
Matthieu Napoli <matthieu@mnapoli.fr>
2021-07-09 15:23:14 +0200

    Improve Storage tests

--- test/unit/storage.test.ts
+++ test/unit/storage.test.ts
@@ -7,8 +7,52 @@ describe("storage", () => {
             configExt: pluginConfigExt,
             command: "package",
         });
-        expect(cfTemplate.Resources[computeLogicalId("avatars", "Bucket")]).toMatchObject({
+        const bucketId = computeLogicalId("avatars", "Bucket");
+        expect(Object.keys(cfTemplate.Resources)).toStrictEqual([
+            "ServerlessDeploymentBucket",
+            "ServerlessDeploymentBucketPolicy",
+            bucketId,
+            computeLogicalId("avatars", "Bucket", "Policy"),
+        ]);
+        expect(cfTemplate.Resources[bucketId]).toStrictEqual({
             Type: "AWS::S3::Bucket",
+            UpdateReplacePolicy: "Retain",
+            DeletionPolicy: "Retain",
+            Properties: {
+                BucketEncryption: {
+                    ServerSideEncryptionConfiguration: [
+                        {
+                            ServerSideEncryptionByDefault: { SSEAlgorithm: "AES256" },
+                        },
+                    ],
+                },
+                LifecycleConfiguration: {
+                    Rules: [
+                        {
+                            Status: "Enabled",
+                            Transitions: [
+                                {
+                                    StorageClass: "INTELLIGENT_TIERING",
+                                    TransitionInDays: 0,
+                                },
+                            ],
+                        },
+                        {
+                            NoncurrentVersionExpirationInDays: 30,
+                            Status: "Enabled",
+                        },
+                    ],
+                },
+                PublicAccessBlockConfiguration: {
+                    BlockPublicAcls: true,
+                    BlockPublicPolicy: true,
+                    IgnorePublicAcls: true,
+                    RestrictPublicBuckets: true,
+                },
+                VersioningConfiguration: {
+                    Status: "Enabled",
+                },
+            },
         });
     });
 });
