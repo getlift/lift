10c44154caabda149c0540e41bc9f5f576e5d7b9
Frédéric Barthelet <fredericb@theodo.fr>
2021-07-12 11:58:09 +0200

    Remove stripe tests

--- test/unit/stripe.test.ts
+++
@@ -1,9 +0,0 @@
-import { StripeProvider } from "../../src/providers";
-
-describe("stripe", () => {
-    it("should source local configuration", () => {
-        // @ts-expect-error no serverless mock
-        const provider = new StripeProvider({});
-        expect(provider.config).toBe(12);
-    });
-});
