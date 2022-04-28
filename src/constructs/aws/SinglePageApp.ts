b89314eba7b82a50537e10811ccd6bf1dbc5a2f9
fargito <fr.farge96@gmail.com>
2022-04-28 10:44:55 +0200

    feat(single-page): add xml to static files regexp

--- src/constructs/aws/SinglePageApp.ts
+++ src/constructs/aws/SinglePageApp.ts
@@ -42,8 +42,9 @@ export class SinglePageApp extends StaticWebsiteAbstract {
          * let static files pass.
          *
          * Files extensions list taken from: https://docs.aws.amazon.com/amplify/latest/userguide/redirects.html#redirects-for-single-page-web-apps-spa
+         * Add xml as well
          */
-        const code = `var REDIRECT_REGEX = /^[^.]+$|\\.(?!(css|gif|ico|jpg|jpeg|js|png|txt|svg|woff|woff2|ttf|map|json)$)([^.]+$)/;
+        const code = `var REDIRECT_REGEX = /^[^.]+$|\\.(?!(css|gif|ico|jpg|jpeg|js|png|txt|svg|woff|woff2|ttf|map|json|xml)$)([^.]+$)/;
 
 function handler(event) {
     var uri = event.request.uri;
