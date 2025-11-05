df507370b62ada821e84cec563ff7ddb695fcc52
Ricardo Nolde <nolde@users.noreply.github.com>
2025-11-05 19:22:32 +1100

    feat: add `.lottie` to the redirect rgx for `single-page-app` construct
    Lottie files are a growing option for online animations.
    https://en.wikipedia.org/wiki/Lottie_(file_format)#The_dotLottie_file_format

--- src/constructs/aws/SinglePageApp.ts
+++ src/constructs/aws/SinglePageApp.ts
@@ -45,7 +45,7 @@ export class SinglePageApp extends StaticWebsiteAbstract {
          * Files extensions list taken from: https://docs.aws.amazon.com/amplify/latest/userguide/redirects.html#redirects-for-single-page-web-apps-spa
          * Add pdf, xml, webmanifest, avif and wasm as well
          */
-        const code = `var REDIRECT_REGEX = /^[^.]+$|\\.(?!(css|gif|ico|jpg|jpeg|js|png|txt|svg|woff|woff2|ttf|map|json|webp|xml|pdf|webmanifest|avif|wasm)$)([^.]+$)/;
+        const code = `var REDIRECT_REGEX = /^[^.]+$|\\.(?!(css|gif|ico|jpg|jpeg|js|png|txt|svg|woff|woff2|ttf|map|json|webp|xml|pdf|webmanifest|avif|wasm|lottie)$)([^.]+$)/;
 
 function handler(event) {
     var uri = event.request.uri;
